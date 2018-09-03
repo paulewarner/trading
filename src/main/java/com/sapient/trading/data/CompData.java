e4e26c148f8f54d0e7562439dd639da7edfb0cbc
davkim2 <davkim2@WKWUS4191978.global.publicisgroupe.net>
2018-09-03 14:41:33 +0530

    added market data files. Search function should be functional.

---
+++ src/main/java/com/sapient/trading/data/CompData.java
@@ -0,0 +1,120 @@
+package com.sapient.trading.data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import pl.zankowski.iextrading4j.api.exception.IEXTradingException;
+import pl.zankowski.iextrading4j.api.refdata.ExchangeSymbol;
+
+public class CompData {
+	/** tickers holds the mapping of a company's name to it's ticker. */
+	Map<String, String> tickers;
+	
+	/** compName holds the names of all the companies listed on IEX. */
+	List<String> compName;
+	
+	/******************************Constructors*******************************/
+	
+	/**
+	 * Initializes a new CompData Object with tickers as a HashMap of 
+	 * <String,String> and compName as an arrayList of <String>
+	 */
+	public CompData() {
+		super();
+		this.tickers = new HashMap<String, String>();
+		this.compName = new ArrayList<String>();
+	}
+	
+	/**********************************Methods********************************/
+	
+	/**
+	 * addData takes in a List of data returned from the 
+	 * IEX api and runs the add method of this class for 
+	 * each of the elements in the list.
+	 * 
+	 * @param esList
+	 * 		  List of data about companies
+	 */
+	public void addData(List<ExchangeSymbol> esList) {
+		esList.stream().forEach(es -> this.add(es));
+		
+	}
+	
+	
+	/**
+	 * add takes in an ExchangeSymbol object and adds data to the 
+	 * Map (tickers) and List (compName)
+	 * tickers holds the company name and the ticker
+	 * compName holds the company name
+	 * 
+	 * @param es
+	 * 		  Data about a company
+	 */
+	public void add(ExchangeSymbol es) {
+		this.tickers.put(es.getName().toLowerCase(), es.getSymbol().toLowerCase());
+		this.compName.add(es.getName().toLowerCase());
+	}
+	
+	/**
+	 * findTickers looks for and returns any company name that contains 
+	 * the string cName.
+	 * 
+	 * @param cName
+	 * 		  Partial name or full name of company
+	 * 
+	 * @return a List of Strings of tickers where the company name contains 
+	 * the string cName
+	 */
+	public List<String> findTickers(String cName) {
+		List<String> results = this.compName.stream().
+				filter(comp -> comp.contains(cName.toLowerCase()))
+				.collect(Collectors.toList());
+		if(results.size() > 10) {
+			results = results.subList(0, 10);
+		}
+		if(results.size() == 0) {
+			throw new IEXTradingException("ERROR");
+		}
+		return results;
+	}
+	
+	/**
+	 * findTicker runs the method this.findTickers(cName) and then returns 
+	 * the first element of the list that the findTickers function returns.
+	 * 
+	 * @param name
+	 * 		  Partial name or full name of company
+	 * 
+	 * @return first element of list of Strings
+	 */
+	public String findTicker(String name) {
+		List<String> results = this.findTickers(name);
+		return this.tickers.get(results.get(0));
+	}
+	
+	/**
+	 * tickers finds a list of tickers that include the partial or full
+	 * ticker in the parameter name.
+	 * 
+	 * @param name
+	 * 		  Partial name or full name of company
+	 * 
+	 * @return first element of list of Strings
+	 */
+	public List<String> tickers(String name) {
+		List<String> results = this.tickers.values().stream().
+				filter(comp -> comp.contains(name.toLowerCase()))
+				.collect(Collectors.toList());
+		if(results.size() > 10) {
+			results = results.subList(0, 10);
+		}
+		if(results.size() == 0) {
+			throw new IEXTradingException("ERROR");
+		}
+		return results;
+	}
+	
+}
---
+++ src/main/java/com/sapient/trading/models/Company.java
@@ -0,0 +1,153 @@
+package com.sapient.trading.models;
+
+import java.math.BigDecimal;
+
+public class Company {
+	private String name;
+	private String tick;
+	private BigDecimal price;
+	private BigDecimal priceChange;
+	private BigDecimal percentChange;
+	
+	private BigDecimal open;
+	private BigDecimal close;
+	private BigDecimal high;
+	private BigDecimal low;
+	private BigDecimal prevClose;
+	private BigDecimal fifty2WeekHigh;
+	private BigDecimal fifty2WeekLow;
+	private BigDecimal peRatio;
+	private BigDecimal avgTotVolume;
+	private BigDecimal mrktCap;
+	private BigDecimal ytdChange;
+	
+	
+	/******************Constructors*************************/
+	public Company(String name, String tick, BigDecimal price, BigDecimal priceChange, BigDecimal percentChange) {
+		super();
+		this.name = name;
+		this.tick = tick;
+		this.price = price;
+		this.priceChange = priceChange;
+		this.percentChange = percentChange;
+	}
+
+
+	/*********************Getters*************************/
+	public String getName() {
+		return name;
+	}
+
+	public String getTick() {
+		return tick;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public BigDecimal getPriceChange() {
+		return priceChange;
+	}
+	
+	public BigDecimal getPercentChange() {
+		return percentChange;
+	}
+	
+	public BigDecimal getOpen() {
+		return open;
+	}
+
+	public BigDecimal getClose() {
+		return close;
+	}
+
+	public BigDecimal getHigh() {
+		return high;
+	}
+
+	public BigDecimal getLow() {
+		return low;
+	}
+
+	public BigDecimal getPrevClose() {
+		return prevClose;
+	}
+
+	public BigDecimal getFifty2WeekHigh() {
+		return fifty2WeekHigh;
+	}
+
+	public BigDecimal getFifty2WeekLow() {
+		return fifty2WeekLow;
+	}
+
+	public BigDecimal getPeRatio() {
+		return peRatio;
+	}
+
+	public BigDecimal getAvgTotVolume() {
+		return avgTotVolume;
+	}
+
+	public BigDecimal getMrktCap() {
+		return mrktCap;
+	}
+
+	public BigDecimal getYtdChange() {
+		return ytdChange;
+	}
+
+	/*******************Setters*************************/
+	public void setOpen(BigDecimal open) {
+		this.open = open;
+	}
+
+	public void setClose(BigDecimal close) {
+		this.close = close;
+	}
+
+	public void setHigh(BigDecimal high) {
+		this.high = high;
+	}
+
+	public void setLow(BigDecimal low) {
+		this.low = low;
+	}
+
+	public void setPrevClose(BigDecimal prevClose) {
+		this.prevClose = prevClose;
+	}
+
+	public void setFifty2WeekHigh(BigDecimal fifty2WeekHigh) {
+		this.fifty2WeekHigh = fifty2WeekHigh;
+	}
+
+	public void setFifty2WeekLow(BigDecimal fifty2WeekLow) {
+		this.fifty2WeekLow = fifty2WeekLow;
+	}
+
+	public void setPeRatio(BigDecimal peRatio) {
+		this.peRatio = peRatio;
+	}
+
+	public void setAvgTotVolume(BigDecimal avgTotVolume) {
+		this.avgTotVolume = avgTotVolume;
+	}
+
+	public void setMrktCap(BigDecimal mrktCap) {
+		this.mrktCap = mrktCap;
+	}
+
+	public void setYtdChange(BigDecimal ytdChange) {
+		this.ytdChange = ytdChange;
+	}
+
+	/*******************Methods*************************/
+	@Override
+	public String toString() {
+		return "Company [name=" + name + ", tick=" + tick + ", price=" + price + ", priceChange=" + priceChange
+				+ ", percentChange=" + percentChange + "]";
+	}
+
+}
