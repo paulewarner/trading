b0d70401abd96fe89762511d19d863ad99a0b24e
primunda <primunda@WKWIN6151002.global.publicisgroupe.net>
2018-09-04 12:56:07 +0530

    Completed back end.. integratioin left

--- src/main/java/com/sapient/trading/controllers/ExecutionManagementController.java
+++ src/main/java/com/sapient/trading/controllers/ExecutionManagementController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.sql.Connection;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 import com.sapient.trading.models.Block;
@@ -26,16 +27,47 @@ public class ExecutionManagementController {
 	ExecMgmtSysRepo EMSRepo;
 	
 	@RequestMapping(path="/forwardOrder", method= RequestMethod.POST)
-	public String handleForwardedOrders(@RequestParam("order") Order order){
+	public String handleForwardedOrders(@RequestParam("order") String orderID){
+		List<Order> li = new ArrayList<Order>();
 		
 //		Retrieving order
-		EMSRepo.retrieveOrder( order)
+		Order tmp = EMSRepo.retrieveOrder( orderID );
+		li.add(tmp);
+		
+//		Creating EMS Service for forwarding 
+		EMSService ems = new EMSService();
+		
+//		Forwarding order
+		li = ems.forwardOrder(li);
+		
+//		Updating database
+		for(Order o : li) {
+			EMSRepo.orderUpdate(o);
+		}
+		
 		return "res";
 	}
 	
 	@RequestMapping(path="/forwardBlock", method= RequestMethod.POST)
-	public String handleForwardedOrders(@RequestParam("block") Block block){
+	public String handleForwardedBlocks(@RequestParam("block") String blockID){
+		List<Order> li = new ArrayList<Order>();
+		
+		
+//		Creating EMS Service for forwarding 
+		EMSService ems = new EMSService();
+		
+//		Populating list of orders
+		li = EMSRepo.retrieveBlock(blockID);
+		
+//		Forwarding order
+		li = ems.forwardOrder(li);
+		
+//		Updating database
+		for(Order o : li) {
+			EMSRepo.orderUpdate(o);
+		}
 		
 		return "res";
 	}
+	
 }
